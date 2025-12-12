32a6e7797ec0010f0fe71b836b322bb386edaf60
Gareth Evans <gareth@bryncynfelin.co.uk>
2025-12-12 09:45:05 +0000

    chore: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.8"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.garethjevans"
+version = "0.0.1-SNAPSHOT"
+description = "Spring Boot Observaility"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
