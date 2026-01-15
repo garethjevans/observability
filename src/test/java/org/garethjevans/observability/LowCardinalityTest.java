4fdd8a3f7ea8f49aea613642b4e274f209f28b6e
Gareth Evans <gareth@bryncynfelin.co.uk>
2026-01-15 15:39:26 +0000

    chore: implement jonatans recommendations

--- src/test/java/org/garethjevans/observability/LowCardinalityTest.java
+++ src/test/java/org/garethjevans/observability/LowCardinalityTest.java
@@ -2,37 +2,24 @@ package org.garethjevans.observability;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.client.HttpClientErrorException;
 
 @SpringBootTest(
     properties = {"server.shutdown=immediate"},
     webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles({"test-data"})
-public class LowCardinalityTest {
+public class LowCardinalityTest extends AbstractCardinalityTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(LowCardinalityTest.class);
-
-  @Autowired protected MockMvc mockMvc;
 
   @Test
   public void canGetMetricsForTestApplication() {
@@ -68,62 +55,6 @@ public class LowCardinalityTest {
       fail("Expected HttpClientErrorException");
     } catch (HttpClientErrorException e) {
       assertThat(e.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-    }
-  }
-
-  private String getLowScrapeEndpoint() {
-    try {
-      MvcResult mockRes =
-          mockMvc.perform(get("/actuator/prometheus").accept(MediaType.TEXT_PLAIN)).andReturn();
-      checkResponseCode(mockRes.getResponse());
-      return mockRes.getResponse().getContentAsString();
-    } catch (HttpClientErrorException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private String getHighScrapeEndpoint() {
-    try {
-      MvcResult mockRes =
-          mockMvc
-              .perform(get("/actuator/prometheus-high-cardinality").accept(MediaType.TEXT_PLAIN))
-              .andReturn();
-      checkResponseCode(mockRes.getResponse());
-      return mockRes.getResponse().getContentAsString();
-    } catch (HttpClientErrorException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private String getActuatorMetricsForTestApplication() {
-    try {
-      MvcResult mockRes =
-          mockMvc
-              .perform(get("/actuator/metrics/test.application").accept(MediaType.APPLICATION_JSON))
-              .andReturn();
-      checkResponseCode(mockRes.getResponse());
-      return mockRes.getResponse().getContentAsString();
-    } catch (HttpClientErrorException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private void checkResponseCode(MockHttpServletResponse response)
-      throws UnsupportedEncodingException {
-    LOGGER.trace(
-        "checking response code: {} - {}", response.getStatus(), response.getContentAsString());
-    if (!HttpStatus.valueOf(response.getStatus()).is2xxSuccessful()) {
-      throw new HttpClientErrorException(
-          HttpStatusCode.valueOf(response.getStatus()),
-          new String(response.getContentAsByteArray()),
-          response.getContentAsByteArray(),
-          StandardCharsets.UTF_8);
     }
   }
 }
